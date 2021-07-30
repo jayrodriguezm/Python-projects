6af25365721af3f5875df35f1e8ce5f9321b6ccb
Jairo Rodríguez Mejías <jayrodriguezm@gmail.com>
2021-07-30 00:38:24 -0600

    Exercise completed

--- CaesarCipher/main.py
+++ CaesarCipher/main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def encrypt(plain_text, shift_number):
+    encrypted_text = ""
+    for letter in plain_text:
+        index = alphabet.index(letter)
+        encrypted_text += alphabet[index - shift_number]
+    return encrypted_text
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def decrypt(plain_text, shift_number):
+    decrypted_text = ""
+    for letter in plain_text:
+        index = alphabet.index(letter)
+        decrypted_text += alphabet[index + shift_number]
+    return decrypted_text
+
+
+if direction == "encode":
+    print("The encoded text is: " + encrypt(text, shift))
+elif direction == "decode":
+    print("The decoded text is: " + decrypt(text, shift))
+else:
+    print("Wrong input for direction")
