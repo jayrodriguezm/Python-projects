9df389433a462bf136722f9cb277b5dba001b317
Jairo Rodríguez Mejías <jayrodriguezm@gmail.com>
2021-07-30 11:37:51 -0600

    Final version after recommendations

--- CaesarCipher/main.py
+++ CaesarCipher/main.py
@@ -1,17 +1,17 @@
-import art
+from art import logo
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 response = "yes"
-print(art.logo)
+print(logo)
 
 
 def caesar(input_text, input_shift, input_direction):
     result_text = ""
-    for letter in input_text:
-        if letter not in alphabet:
-            result_text += letter
+    for character in input_text:
+        if character not in alphabet:
+            result_text += character
         else:
-            index = alphabet.index(letter)
+            index = alphabet.index(character)
             if input_direction == "encode":
                 result_text += alphabet[index - input_shift]
             elif input_direction == "decode":
@@ -23,11 +23,12 @@ def main_menu():
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    if shift > 26:
-        shift %= 26
+    shift %= 26
     caesar(text, shift, direction)
 
 
 while response == "yes":
     main_menu()
     response = input("Type 'yes' if you want to go aengain. Otherwise type 'no'.\n")
+
+print("Goodbye")
