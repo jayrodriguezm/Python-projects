4086d2459e3a91e9e19d6f5ba26c95139f7244a1
Jairo Rodríguez Mejías <jayrodriguezm@gmail.com>
2021-08-03 23:16:04 -0600

    Added docstrings

--- SecretAuction/main.py
+++ SecretAuction/main.py
@@ -8,17 +8,20 @@ response = "yes"
 
 
 def add_bidder(bidder_name, bidder_bid):
+    """Adds a bidder to the dictionary"""
     new_bidder = {"name": bidder_name, "bid": bidder_bid}
     bidders.append(new_bidder)
 
 
 def request_bidder_info():
+    """Requests the bidder's info to the user"""
     name = input("What is your name?: ")
     bid = input("What's your bid?: $")
     add_bidder(name, bid)
 
 
 def determine_winner():
+    """Determines who the highest bidder is"""
     highest_bidder = {"name": "None", "bid": 0}
     for bidder in bidders:
         if int(bidder["bid"]) > int(highest_bidder["bid"]):
